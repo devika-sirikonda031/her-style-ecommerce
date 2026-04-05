267e16d4ec2d112f47600ce134b9b40e36f50d19
Devika Sirikonda <sirikondadevika031@gmail.com>
2026-04-05 22:50:47 +0530

    added debug logs for getProducts

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -38,17 +38,18 @@ export const getProducts = async (req, res) => {
 
     const products = await Product.find();
 
-    console.log("Products:", products); // 👈 SEE THIS IN RENDER LOGS
+    console.log("Products:", products);
 
     res.status(200).json({
       products: products
     });
 
   } catch (error) {
-    console.log("❌ ERROR:", error.message); // 👈 VERY IMPORTANT
+    console.log("❌ FULL ERROR:", error); // ✅ THIS LINE
     res.status(500).json({ message: "Error fetching products ❌" });
   }
 };
+
 
 
 // ❌ DELETE PRODUCT
