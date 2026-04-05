1298af1f5c4fc500add5c77a23e6b4dc591ac22b
Devika Sirikonda <sirikondadevika031@gmail.com>
2026-04-05 15:19:23 +0530

    fixed server for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CartProvider>
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <App />
-      </BrowserRouter>
+      </BrowserRouter> */}
+      <HashRouter>
+        <App />
+      </HashRouter>
     </CartProvider>
   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/her-style-ecommerce/',  // ✅ VERY IMPORTANT
+  base: '/her-style-ecommerce/',
 })
