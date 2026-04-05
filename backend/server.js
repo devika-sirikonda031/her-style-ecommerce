c46fece8566fd0cb9a4ee3510828cea2cce51eaa
Devika Sirikonda <sirikondadevika031@gmail.com>
2026-04-05 17:16:55 +0530

    fixed module path

--- backend/server.js
+++ backend/server.js
@@ -9,22 +9,15 @@ dotenv.config();
 
 const app = express();
 
-// MIDDLEWARE
 app.use(cors());
 app.use(express.json());
 
-// ROUTES
 app.use("/api/products", productRoutes);
 
-// DB CONNECT
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected ✅"))
   .catch((err) => console.log(err));
 
-// ✅ PORT (FIX)
-const PORT = process.env.PORT || 5000;
-
-// ✅ SERVER
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT} 🚀`);
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server running 🚀");
 });
